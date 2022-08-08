3853a4472f5ae388ad12f116e3cba4b03b7a49ef
Ilya Kaplunov <ilya04-2000@main.ru>
2022-08-09 00:03:11 +0800

    Updates

--- js/main.js
+++ js/main.js
@@ -211,33 +211,19 @@ if (burger) {
   \***************************************/
 /***/ (() => {
 
-function _createForOfIteratorHelper(o, allowArrayLike) { var it = typeof Symbol !== "undefined" && o[Symbol.iterator] || o["@@iterator"]; if (!it) { if (Array.isArray(o) || (it = _unsupportedIterableToArray(o)) || allowArrayLike && o && typeof o.length === "number") { if (it) o = it; var i = 0; var F = function F() {}; return { s: F, n: function n() { if (i >= o.length) return { done: true }; return { done: false, value: o[i++] }; }, e: function e(_e) { throw _e; }, f: F }; } throw new TypeError("Invalid attempt to iterate non-iterable instance.\nIn order to be iterable, non-array objects must have a [Symbol.iterator]() method."); } var normalCompletion = true, didErr = false, err; return { s: function s() { it = it.call(o); }, n: function n() { var step = it.next(); normalCompletion = step.done; return step; }, e: function e(_e2) { didErr = true; err = _e2; }, f: function f() { try { if (!normalCompletion && it.return != null) it.return(); } finally { if (didErr) throw err; } } }; }
+var showMore = document.querySelector('.team__show');
+var productsLength = document.querySelectorAll('.team__item').length;
+var items = 6;
+showMore.addEventListener('click', function () {
+  items += 3;
+  var array = Array.from(document.querySelector('.team__inner').children);
+  var visItems = array.slice(0, items);
+  visItems.forEach(function (el) {
+    return el.classList.add('is-visible');
+  });
 
-function _unsupportedIterableToArray(o, minLen) { if (!o) return; if (typeof o === "string") return _arrayLikeToArray(o, minLen); var n = Object.prototype.toString.call(o).slice(8, -1); if (n === "Object" && o.constructor) n = o.constructor.name; if (n === "Map" || n === "Set") return Array.from(o); if (n === "Arguments" || /^(?:Ui|I)nt(?:8|16|32)(?:Clamped)?Array$/.test(n)) return _arrayLikeToArray(o, minLen); }
-
-function _arrayLikeToArray(arr, len) { if (len == null || len > arr.length) len = arr.length; for (var i = 0, arr2 = new Array(len); i < len; i++) { arr2[i] = arr[i]; } return arr2; }
-
-var showButton = document.querySelector(".team__show");
-var itemNone = document.querySelectorAll(".team__item--none");
-showButton.addEventListener("click", function (e) {
-  var _iterator = _createForOfIteratorHelper(itemNone),
-      _step;
-
-  try {
-    for (_iterator.s(); !(_step = _iterator.n()).done;) {
-      var item = _step.value;
-      item.classList.toggle("_block");
-
-      if (item.classList.contains("_block")) {
-        showButton.innerHTML = "\u0421\u0432\u0435\u0440\u043D\u0443\u0442\u044C";
-      } else {
-        showButton.innerHTML = "\u0412\u0441\u044F \u043A\u043E\u043C\u0430\u043D\u0434\u0430";
-      }
-    }
-  } catch (err) {
-    _iterator.e(err);
-  } finally {
-    _iterator.f();
+  if (visItems.length === productsLength) {
+    showMore.style.display = 'none';
   }
 });
 
